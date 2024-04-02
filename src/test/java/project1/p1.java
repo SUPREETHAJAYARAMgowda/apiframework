1bf2fdc80e703672ec61a8fbf95344d44f3cf29a
SUPREETHAJAYARAMgowda <s4supritha.gowda@gmail.com>
2024-04-02 17:26:21 +0530

    commited

---
+++ src/test/java/project1/p1.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class p1 {
+
+	public p1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/project2/p2.java
@@ -0,0 +1,14 @@
+package project2;
+
+public class p2 {
+
+	public p2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
